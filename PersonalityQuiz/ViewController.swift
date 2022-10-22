6bfdc3987769e7d4eb734ad7a218f4551d4cd2cc
darsh1192 <dnp87@scarletmail.rutgers.edu>
2022-10-22 11:23:28 -0400

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Darsh Patel on 10/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
